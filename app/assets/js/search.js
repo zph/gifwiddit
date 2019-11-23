dfb1928edf51619c870214a4c85369385a7ee79c
Zander Hill <zander@xargs.io>
2019-11-23 18:13:55 -0500

    Autoformatting w/ prettier

--- app/assets/js/search.js
+++ app/assets/js/search.js
@@ -1,108 +1,115 @@
-const {ipcRenderer} = require('electron')
+const {ipcRenderer} = require('electron');
 
-var searchInput = document.querySelector('.js-search')
-var title = document.querySelector('h1')
-var images = []
+var searchInput = document.querySelector('.js-search');
+var title = document.querySelector('h1');
+var images = [];
 
-var searching = false
+var searching = false;
 
 function isCompatibleURL(str) {
-  var pattern = /^https?:\/\/(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png|bmp)$/i
+  var pattern = /^https?:\/\/(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png|bmp)$/i;
   return pattern.test(str);
 }
 
-function search (query) {
+function search(query) {
   if (searching) {
-    clearTimeout(searching)
+    clearTimeout(searching);
   }
-  if(query.trim() == '') {
-    addImageResults(images)
+  if (query.trim() == '') {
+    addImageResults(images);
   } else {
-    searching = setTimeout(function () {
-      var results = images.filter(function(image){
-        return ((image.keywords.match(query) || image.url == query) && image)
+    searching = setTimeout(function() {
+      var results = images.filter(function(image) {
+        return (image.keywords.match(query) || image.url == query) && image;
       });
-      addImageResults(results)
-      if(results.length == 0) {
-        if(isCompatibleURL(query)) {
+      addImageResults(results);
+      if (results.length == 0) {
+        if (isCompatibleURL(query)) {
           setupAddNewImage(query);
         }
       }
-    }, 100)
+    }, 100);
   }
 }
 
 function addToLibrary() {
-  var url = searchInput.value
-  var tags = document.getElementById('tags').value
+  var url = searchInput.value;
+  var tags = document.getElementById('tags').value;
 
   var obj = {
-    'url': url,
-    'keywords': tags
-  }
+    url: url,
+    keywords: tags,
+  };
 
-  ipcRenderer.send("add-to-library",obj)
+  ipcRenderer.send('add-to-library', obj);
 }
 
-function sizeInBytes(url,elem) {
+function sizeInBytes(url, elem) {
   var xhr = new XMLHttpRequest();
   xhr.open('HEAD', url, true);
-  xhr.onreadystatechange = function(){
-    if ( xhr.readyState == 4 ) {
-      if ( xhr.status == 200 ) {
-        elem.innerHTML = fileSizeSI(xhr.getResponseHeader('Content-Length'))
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        elem.innerHTML = fileSizeSI(xhr.getResponseHeader('Content-Length'));
       }
     }
   };
   xhr.send(null);
 }
 
-function fileSizeSI(a,b,c,d,e){
- return (b=Math,c=b.log,d=1e3,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)
- +' '+(e?'kMGTPEZY'[--e]+'B':'Bytes')
+function fileSizeSI(a, b, c, d, e) {
+  return (
+    ((b = Math),
+    (c = b.log),
+    (d = 1e3),
+    (e = (c(a) / c(d)) | 0),
+    a / b.pow(d, e)).toFixed(2) +
+    ' ' +
+    (e ? 'kMGTPEZY'[--e] + 'B' : 'Bytes')
+  );
 }
 
 function setupAddNewImage(query) {
-  var button = document.createElement("button")
-  button.innerText = "Add to library.."
-  button.setAttribute("id","add-to-library");
-  button.setAttribute("onclick","addToLibrary()")
+  var button = document.createElement('button');
+  button.innerText = 'Add to library..';
+  button.setAttribute('id', 'add-to-library');
+  button.setAttribute('onclick', 'addToLibrary()');
 
-  var input = document.createElement("input")
-  input.setAttribute("id","tags")
-  input.setAttribute("type","text")
+  var input = document.createElement('input');
+  input.setAttribute('id', 'tags');
+  input.setAttribute('type', 'text');
 
-  var text1 = document.createElement("p")
-  text1.innerText = "Enter tags "
-  text1.appendChild(input)
+  var text1 = document.createElement('p');
+  text1.innerText = 'Enter tags ';
+  text1.appendChild(input);
 
-  var text2 = document.createElement("p")
-  text2.innerText = "Hit enter to "
-  text2.appendChild(button)
+  var text2 = document.createElement('p');
+  text2.innerText = 'Hit enter to ';
+  text2.appendChild(button);
 
-  document.getElementById('images').appendChild(text1)
-  document.getElementById('images').appendChild(text2)
+  document.getElementById('images').appendChild(text1);
+  document.getElementById('images').appendChild(text2);
 }
 
 function addImageResult(element, index, array) {
-  var elem = document.createElement("img");
-  elem.setAttribute("src", element.url);
-  elem.setAttribute("width", "100%");
+  var elem = document.createElement('img');
+  elem.setAttribute('src', element.url);
+  elem.setAttribute('width', '100%');
 
-  var wrap = document.createElement("a");
-  wrap.setAttribute("class","wrapper");
-  wrap.setAttribute("href","#");
-  wrap.addEventListener("click",killEvent)
-  wrap.addEventListener("dblclick",sendImageDblClick);
+  var wrap = document.createElement('a');
+  wrap.setAttribute('class', 'wrapper');
+  wrap.setAttribute('href', '#');
+  wrap.addEventListener('click', killEvent);
+  wrap.addEventListener('dblclick', sendImageDblClick);
 
-  var tags = document.createElement("p");
-  tags.setAttribute("class","tags");
-  tags.innerText = element.keywords
+  var tags = document.createElement('p');
+  tags.setAttribute('class', 'tags');
+  tags.innerText = element.keywords;
 
-  var sizeElem = document.createElement("span")
-  sizeElem.setAttribute("class","filesize")
+  var sizeElem = document.createElement('span');
+  sizeElem.setAttribute('class', 'filesize');
 
-  sizeInBytes(element.url,sizeElem);
+  sizeInBytes(element.url, sizeElem);
 
   wrap.appendChild(elem);
   wrap.appendChild(sizeElem);
@@ -121,78 +128,79 @@ function sendImageDblClick(event) {
 }
 
 function sendImage(element) {
-  ipcRenderer.send("url-to-clipboard",element.src);
+  ipcRenderer.send('url-to-clipboard', element.src);
 }
 
 function deleteImage(element) {
-  ipcRenderer.send("remove-from-library",element.src);
+  ipcRenderer.send('remove-from-library', element.src);
 }
 
 function addImageResults(data) {
-  var node = document.getElementById("images");
+  var node = document.getElementById('images');
   var last;
-  while (last = node.lastChild) node.removeChild(last);
-  data.forEach(addImageResult)
+  while ((last = node.lastChild)) node.removeChild(last);
+  data.forEach(addImageResult);
 }
 
-searchInput.focus()
-searchInput.addEventListener('input', function (event) {
-  search(this.value)
-})
+searchInput.focus();
+searchInput.addEventListener('input', function(event) {
+  search(this.value);
+});
 
-document.getElementById('quit').addEventListener('click', function (event) {
-  ipcRenderer.send("quit")
-})
+document.getElementById('quit').addEventListener('click', function(event) {
+  ipcRenderer.send('quit');
+});
 
-document.getElementById('select-library').addEventListener('click', function (event) {
-  ipcRenderer.send("select-library")
-})
+document
+  .getElementById('select-library')
+  .addEventListener('click', function(event) {
+    ipcRenderer.send('select-library');
+  });
 
-document.addEventListener('keydown', function (event) {
-  var node = document.getElementById("images")
+document.addEventListener('keydown', function(event) {
+  var node = document.getElementById('images');
   var first = node.querySelector('a.wrapper');
-  if(event.keyCode == 38) {
-    if(event.target.className.match('wrapper')) {
+  if (event.keyCode == 38) {
+    if (event.target.className.match('wrapper')) {
       element = event.target.previousSibling;
       element.focus();
       element.scrollIntoView();
-      killEvent(event)
+      killEvent(event);
     }
-  } else if(event.keyCode == 40) {
-    if(event.target.className.match('js-search')) {
+  } else if (event.keyCode == 40) {
+    if (event.target.className.match('js-search')) {
       first.focus();
-      killEvent(event)
+      killEvent(event);
     } else if (event.target.className.match('wrapper')) {
       element = event.target.nextSibling;
       element.focus();
       element.scrollIntoView();
-      killEvent(event)
+      killEvent(event);
     }
-  } else if(event.keyCode == 13) {
-    if(event.target.className.match('wrapper')) {
+  } else if (event.keyCode == 13) {
+    if (event.target.className.match('wrapper')) {
       sendImage(event.target.children[0]);
     } else if (event.target.id == 'tags') {
       addToLibrary();
     }
-  } else if(event.keyCode == 8) {
-    if(event.target.className.match('wrapper')) {
-      deleteImage(event.target.children[0])
+  } else if (event.keyCode == 8) {
+    if (event.target.className.match('wrapper')) {
+      deleteImage(event.target.children[0]);
     }
   }
 });
 
-
-ipcRenderer.on('data-added', function(event,data) {
-  images = data
-  searchInput.value = ""
-  search('')
+ipcRenderer.on('data-added', function(event, data) {
+  images = data;
+  searchInput.value = '';
+  search('');
 });
 
-ipcRenderer.on('show', function () {
+ipcRenderer.on('show', function() {
   searchInput.focus();
-  window.scrollTo(0,0)
+  window.scrollTo(0, 0);
 });
 
 ipcRenderer.on('save-error', function(err) {
-  alert("Error saving - "+err)
+  alert('Error saving - ' + err);
 });
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const {dialog,shell,ipcMain,clipboard,globalShortcut,Menu} = require('electron')
+const {
+  dialog,
+  shell,
+  ipcMain,
+  clipboard,
+  globalShortcut,
+  Menu,
+} = require('electron');
 
 var fs = require('fs');
 var path = require('path');
 var {menubar} = require('menubar');
 
-var mb = menubar({dir: __dirname + '/app', width: 300, preloadWindow: true})
+var mb = menubar({dir: __dirname + '/app', width: 300, preloadWindow: true});
 
 var menuTemplate = [
   {
@@ -13,162 +20,178 @@ var menuTemplate = [
       {
         label: 'Undo',
         accelerator: 'Command+Z',
-        selector: 'undo:'
+        selector: 'undo:',
       },
       {
         label: 'Redo',
         accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
+        selector: 'redo:',
       },
       {
         label: 'Cut',
         accelerator: 'Command+X',
-        selector: 'cut:'
+        selector: 'cut:',
       },
       {
         label: 'Copy',
         accelerator: 'Command+C',
-        selector: 'copy:'
+        selector: 'copy:',
       },
       {
         label: 'Paste',
         accelerator: 'Command+V',
-        selector: 'paste:'
+        selector: 'paste:',
       },
       {
         label: 'Select All',
         accelerator: 'Command+A',
-        selector: 'selectAll:'
+        selector: 'selectAll:',
       },
       {
         label: 'Quit App',
         accelerator: 'Command+Q',
-        selector: 'terminate:'
+        selector: 'terminate:',
       },
       {
         label: 'Toggle DevTools',
         accelerator: 'Alt+Command+I',
-        click: function () { mb.window.toggleDevTools() }
-      }
-    ]
-  }
-]
+        click: function() {
+          mb.window.toggleDevTools();
+        },
+      },
+    ],
+  },
+];
 
-mb.app.on('will-quit', function () {
-  globalShortcut.unregisterAll()
-})
+mb.app.on('will-quit', function() {
+  globalShortcut.unregisterAll();
+});
 
-mb.on('after-create-window', function () {
-  var filepath = path.join(mb.app.getPath('userData'), 'library.gifwit')
+mb.on('after-create-window', function() {
+  var filepath = path.join(mb.app.getPath('userData'), 'library.gifwit');
 
   mb.window.webContents.on('did-finish-load', function() {
     try {
       //test to see if settings exist
       fs.openSync(filepath, 'r+'); //throws error if file doesn't exist
-      var data=fs.readFileSync(filepath); //file exists, get the contents
+      var data = fs.readFileSync(filepath); //file exists, get the contents
       mb.gifwit = JSON.parse(data); //turn to js object
-      mb.window.webContents.send('data-added',mb.gifwit.images);
+      mb.window.webContents.send('data-added', mb.gifwit.images);
     } catch (err) {
       try {
         var fd = fs.openSync(filepath, 'w+');
-        mb.gifwit = {version: 1,images:[]}
+        mb.gifwit = {version: 1, images: []};
       } catch (err) {
         throw err;
       }
     }
-  })
-})
+  });
+});
 
-mb.on('ready', function ready () {
-  var menu = Menu.buildFromTemplate(menuTemplate)
-  var filepath = path.join(mb.app.getPath('userData'), 'library.gifwit')
+mb.on('ready', function ready() {
+  var menu = Menu.buildFromTemplate(menuTemplate);
+  var filepath = path.join(mb.app.getPath('userData'), 'library.gifwit');
 
-  Menu.setApplicationMenu(menu)
+  Menu.setApplicationMenu(menu);
 
-  ipcMain.on('url-to-clipboard',function(data,returnVal) {
+  ipcMain.on('url-to-clipboard', function(data, returnVal) {
     clipboard.writeText(returnVal);
     mb.hideWindow();
-  })
+  });
 
-  ipcMain.on('quit',function () {
-    mb.app.quit()
-  })
+  ipcMain.on('quit', function() {
+    mb.app.quit();
+  });
 
-  ipcMain.on('select-library', function () {
-    dialog.showOpenDialog({properties: ['openFile'],message:"Locate Gifwit library file", filters:[{name: 'GifWit Library', extensions: ['gifwit']}]},(fileNames) => {
+  ipcMain.on('select-library', function() {
+    dialog.showOpenDialog(
+      {
+        properties: ['openFile'],
+        message: 'Locate Gifwit library file',
+        filters: [{name: 'GifWit Library', extensions: ['gifwit']}],
+      },
+      fileNames => {
         // fileNames is an array that contains all the selected
-        if(fileNames === undefined){
-            alert("No library file selected");
-            return;
+        if (fileNames === undefined) {
+          alert('No library file selected');
+          return;
         }
 
-        libraryPath = fileNames[0]
-        savePath = path.join(mb.app.getPath('userData'), 'library.gifwit')
+        libraryPath = fileNames[0];
+        savePath = path.join(mb.app.getPath('userData'), 'library.gifwit');
 
-        if(libraryPath != undefined) {
+        if (libraryPath != undefined) {
           fs.readFile(libraryPath, 'utf-8', (err, data) => {
-              if(err){
-                  alert("An error ocurred reading the file :" + err.message);
+            if (err) {
+              alert('An error ocurred reading the file :' + err.message);
+              return;
+            }
+            var shouldOverwrite = dialog.showMessageBox({
+              type: 'question',
+              title: 'Are you sure?',
+              buttons: ['Yes', 'No'],
+              message:
+                'This will overwrite your existng library, are you sure?',
+            });
+            if (shouldOverwrite == 0) {
+              fs.writeFile(savePath, data, function(err2) {
+                if (err2) {
+                  alert('An error ocurred writing the file :' + err2.message);
                   return;
-              }
-              var shouldOverwrite = dialog.showMessageBox({type: "question",title:"Are you sure?",buttons: ["Yes","No"],message: "This will overwrite your existng library, are you sure?"})
-              if(shouldOverwrite == 0) {
-                fs.writeFile (savePath, data, function(err2) {
-                  if(err2){
-                      alert("An error ocurred writing the file :" + err2.message);
-                      return;
-                  }
-                });
+                }
+              });
 
-                mb.gifwit = JSON.parse(data); //turn to js object
-                mb.window.webContents.send('data-added',mb.gifwit.images);
-              }
+              mb.gifwit = JSON.parse(data); //turn to js object
+              mb.window.webContents.send('data-added', mb.gifwit.images);
+            }
           });
         } else {
-          alert("No library file selected")
+          alert('No library file selected');
         }
-    });
-  })
+      },
+    );
+  });
 
-  ipcMain.on('add-to-library',function(data,returnVal) {
-    mb.gifwit.images.push(returnVal)
+  ipcMain.on('add-to-library', function(data, returnVal) {
+    mb.gifwit.images.push(returnVal);
     try {
       var fd = fs.openSync(filepath, 'w+');
-      fs.writeSync(fd,JSON.stringify(mb.gifwit))
-      mb.window.webContents.send('data-added',mb.gifwit.images);
+      fs.writeSync(fd, JSON.stringify(mb.gifwit));
+      mb.window.webContents.send('data-added', mb.gifwit.images);
+    } catch (err) {
+      mb.window.webContents.send('save-error', err);
     }
-    catch (err) {
-      mb.window.webContents.send('save-error',err);
-    }
+  });
 
-  })
+  ipcMain.on('remove-from-library', function(data, returnVal) {
+    var canDelete = dialog.showMessageBox({
+      type: 'question',
+      title: 'Are you sure?',
+      buttons: ['Yes', 'No'],
+      message: "Are you sure you wan't to delete this image?",
+    });
 
-  ipcMain.on('remove-from-library',function(data,returnVal) {
-    var canDelete = dialog.showMessageBox({type: "question",title:"Are you sure?",buttons: ["Yes","No"],message: "Are you sure you wan't to delete this image?"})
-
-    if(canDelete == 0) {
-      mb.gifwit.images = mb.gifwit.images.filter(function(image){
-        return (image.url != returnVal && image)
+    if (canDelete == 0) {
+      mb.gifwit.images = mb.gifwit.images.filter(function(image) {
+        return image.url != returnVal && image;
       });
 
       try {
         var fd = fs.openSync(filepath, 'w+');
-        fs.writeSync(fd,JSON.stringify(mb.gifwit))
-        window.webContents.send('data-added',mb.gifwit.images);
-      }
-      catch (err) {
-        window.webContents.send('save-error',err);
+        fs.writeSync(fd, JSON.stringify(mb.gifwit));
+        window.webContents.send('data-added', mb.gifwit.images);
+      } catch (err) {
+        window.webContents.send('save-error', err);
       }
     }
-  })
+  });
 
-  mb.on('show', function () {
+  mb.on('show', function() {
     mb.window.webContents.send('show');
   });
 
-    // Register a 'ctrl+shift+space' shortcut listener.
-  var ret = globalShortcut.register('command+shift+g', function () {
-    mb.window.isVisible() ? mb.hideWindow() : mb.showWindow()
-  })
-
+  // Register a 'ctrl+shift+space' shortcut listener.
+  var ret = globalShortcut.register('command+shift+g', function() {
+    mb.window.isVisible() ? mb.hideWindow() : mb.showWindow();
+  });
 });
